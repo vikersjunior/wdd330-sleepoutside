a8f01beb29d4f5ff5cab728d37fc4f1724b05551
Vikers Junior <vikersjunior@gmail.com>
2026-07-08 15:20:09 +0000

    Complete W02 Team Activity

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,48 @@
+import { setLocalStorage, getLocalStorage } from './utils.mjs';
+
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
+
+  async init() {
+    this.product = await this.dataSource.findProductById(this.productId);
+    this.renderProductDetails("main");
+    document.getElementById('addToCart')
+      .addEventListener('click', this.addToCart.bind(this));
+  }
+
+  addToCart() {
+    let cartItems = getLocalStorage("so-cart");
+    if (!Array.isArray(cartItems)) {
+      cartItems = [];
+    }
+    cartItems.push(this.product);
+    setLocalStorage("so-cart", cartItems);
+  }
+
+  renderProductDetails(selector) {
+    const element = document.querySelector(selector);
+    element.innerHTML = `
+      <section class="product-detail">
+        <h3>${this.product.Brand.Name}</h3>
+        <h2 class="divider">${this.product.NameWithoutBrand}</h2>
+        <img
+          class="divider"
+          src="${this.product.Image}"
+          alt="${this.product.NameWithoutBrand}"
+        />
+        <p class="product-card__price">$${this.product.FinalPrice}</p>
+        <p class="product__color">${this.product.Colors[0].ColorName}</p>
+        <p class="product__description">
+          ${this.product.DescriptionHtmlSimple}
+        </p>
+        <div class="product-detail__add">
+          <button id="addToCart" data-id="${this.product.Id}">Add to Cart</button>
+        </div>
+      </section>
+    `;
+  }
+}
